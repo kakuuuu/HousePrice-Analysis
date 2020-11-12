81fd6d39a53c758a6790c855449c91332d8e360d
unknown <1245208481@qq.com>
2020-11-12 18:15:00 +0800

    解决部署项目跨域问题

--- server/app.js
+++ server/app.js
@@ -13,11 +13,24 @@ var updateHouse=require('./routes/update');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  // res.header('Access-Control-Allow-Headers', 'Content-Type');
+  // res.header('Access-Control-Allow-Credentials','true');
+  next();
+};
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//中间件
+// 中间件
+
+// cors
+app.use(allowCrossDomain);
+
 // 日志
 app.use(logger('dev'));
 
