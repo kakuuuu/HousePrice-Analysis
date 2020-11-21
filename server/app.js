b6fa5a96699b125e14571cb114431793013080a0
unknown <1245208481@qq.com>
2020-11-21 16:04:35 +0800

    后端cors请求头添加字段

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ app.use(Sentry.Handlers.requestHandler());
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,ContentType');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,ContentType,authorization');
   // res.header('Access-Control-Allow-Credentials','true');
   next();
 };
